c2fb39a2a0853221ab6371041317497026f37b8c
Vinh Nguyen <vnguye34@students.kennesaw.edu>
2018-11-08 01:15:26 -0500

    finish POC for ROS message filters

--- jet_data_collection/src/test_time_data_pub.py
+++ jet_data_collection/src/test_time_data_pub.py
@@ -8,22 +8,22 @@ global x
 
 def publisher1(publish):
     pub1 = rospy.Publisher('arduino/encoder_left_value', Int64, queue_size = 10)
-    x=random.random()
+    x=1000*(random.random())
     pub1.publish(x)
     
 def publisher2(publish):
     pub2 = rospy.Publisher('arduino/encoder_right_value', Int64, queue_size = 10)
-    x=random.random()
+    x=100*random.random()
     pub2.publish(x)
     
 def publisher3(publish):
     pub3 = rospy.Publisher('arduino/motor_left_speed', Int64, queue_size = 10)
-    x=random.random()
+    x=10000000*random.random()
     pub3.publish(x)
     
 def publisher4(publish):
     pub4 = rospy.Publisher('arduino/motor_right_speed', Int64, queue_size = 10)
-    x=random.random()
+    x=100*random.random()
     pub4.publish(x)
     
 rospy.Timer(rospy.Duration(.1), publisher1)
